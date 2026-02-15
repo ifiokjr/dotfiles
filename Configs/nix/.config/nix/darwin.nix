8aa30d6f25f3046317d660291941b733a35ca066
Ifiok Jr. <ifiokotung@gmail.com>
2026-02-15 10:49:50 +0000

    fix(nix): use list for pathsToLink in buildEnv activation script
    pkgs.buildEnv requires pathsToLink to be a list, not a string.
    Passing "/Applications" as a string causes builder.pl to fail with:
    "Can't use string as an ARRAY ref while strict refs in use"

--- Configs/nix/.config/nix/darwin.nix
+++ Configs/nix/.config/nix/darwin.nix
@@ -124,7 +124,7 @@ in
       env = pkgs.buildEnv {
         name = "system-applications";
         paths = config.environment.systemPackages;
-        pathsToLink = "/Applications";
+        pathsToLink = [ "/Applications" ];
       };
     in
     pkgs.lib.mkForce ''
